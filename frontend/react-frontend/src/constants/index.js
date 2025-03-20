870183c3704e1e8d062d50a99cddb823999048ea
Swayam Pradeep Nakshane <nakshaneswayamatha24@gmail.com>
2025-03-20 21:51:46 +0530

    Delete frontend/react-frontend directory

--- frontend/react-frontend/src/constants/index.js
+++
@@ -1,9 +0,0 @@
-export const API_BASE_URL = process.env.REACT_APP_BACKEND_URL;
-export const FRONTEND_BASE_URL = process.env.REACT_APP_FRONTEND_URL;
-export const ACCESS_TOKEN = 'accessToken'
-
-
-
-export const OAUTH2_REDIRECT_URI = FRONTEND_BASE_URL + '/oauth2/redirect'
-export const GOOGLE_AUTH_URL = API_BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
-export const GOOGLE_AUTH_URL_DJANGO = API_BASE_URL + '/api/oauth2/login/google';
